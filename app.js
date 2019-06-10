aa8c387fe610d2fecf99b130022a23d13afcf37b
bpdavitt <bpdavitt@gmail.com>
2019-06-10 12:20:09 -0500

    Complete min reqs

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+const App = () => (
+    <div>
+        <GroceryList />
+    </div>
+);
+
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    handleMouseOver (event){
+        event.target.style.fontWeight = 'bold';
+        event.target.style.cursor = 'pointer';
+    }
+
+    handleMouseOut (event){
+        event.target.style.fontWeight = '';
+    }
+    
+    render (){
+        return (
+            <li onMouseOver = {this.handleMouseOver.bind(this)} 
+            onMouseOut = {this.handleMouseOut.bind(this)}>
+                {this.props.item}
+            </li>
+        )
+    }
+}
+
+const GroceryList = () => {
+    var items = ['Beer', 'Ice Cream', 'Kale', 'Cheese'];
+    var list = items.map(item => {
+        return(
+            <GroceryListItem item = {item} />
+        )
+    })
+
+    return (
+        <div>
+            <ul>
+                {list}
+            </ul>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
